d5c3a5184ec9d5cb1dd257ae8cd93ebdbb1d11d9
Steve Tripp <stevetripp@comcast.net>
2018-10-10 19:47:36 -0600

    Remove trunk.

---
+++ TNT.Collections/GraphNode.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TNT.Collections
+{
+	/// <summary>
+	/// Represents a graph node where the object on the node is type N and the edges have
+	/// objects of type E
+	/// </summary>
+	/// <typeparam name="N">Node's object type</typeparam>
+	/// <typeparam name="E">Edge's object type</typeparam>
+	public class GraphNode<N, E>
+	{
+		/// <summary>
+		/// Object bound to the node
+		/// </summary>
+		public N Object { get; protected set; }
+
+		/// <summary>
+		/// Edges connected to this node
+		/// </summary>
+		public List<GraphEdge<E, N>> Edges { get; internal set; }
+
+		internal bool Visited { get; set; }
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructor. Initializes the node's object with obj
+		/// </summary>
+		/// <exception cref="ArgumentNullException">Thrown when obj is null</exception>
+		/// <param name="obj">Object to set on the node</param>
+		public GraphNode(N obj)
+		{
+			if ((object)obj == null)
+			{
+				throw new ArgumentNullException("obj");
+			}
+
+			Object = obj;
+			Edges = new List<GraphEdge<E, N>>();
+		}
+
+		#endregion
+
+		#region Equality Methods
+
+		/// <summary>
+		/// Operator == for objects of type GraphNode&lt;N, E&gt;
+		/// </summary>
+		/// <param name="gn1">First comparer</param>
+		/// <param name="gn2">Second comparer</param>
+		/// <returns>True if equal, false otherwise</returns>
+		public static bool operator ==(GraphNode<N, E> gn1, GraphNode<N, E> gn2)
+		{
+			if (ReferenceEquals(gn1, gn2))
+			{
+				return true;
+			}
+
+			if ((object)gn1 == null || (object)gn2 == null)
+			{
+				return false;
+			}
+
+			object o1 = gn1.Object as object;
+			object o2 = gn2.Object as object;
+
+			return o1 == o2;
+		}
+
+		/// <summary>
+		/// Operator != for objects of type GraphNode&lt;N, E&gt;
+		/// </summary>
+		/// <param name="gn1">First comparer</param>
+		/// <param name="gn2">Second comparer</param>
+		/// <returns>True if not equal, false otherwise</returns>
+		public static bool operator !=(GraphNode<N, E> gn1, GraphNode<N, E> gn2)
+		{
+			return !(gn1 == gn2);
+		}
+
+		/// <summary>
+		/// Operator == for objects of type GraphNode&lt;N, E&gt; and type N
+		/// </summary>
+		/// <param name="gn">First comparer</param>
+		/// <param name="obj">Second comparer</param>
+		/// <returns>True if equal, false otherwise</returns>
+		public static bool operator ==(GraphNode<N, E> gn, N obj)
+		{
+			if ((object)gn == null || (object)obj == null)
+			{
+				return false;
+			}
+
+			object o1 = gn.Object as object;
+			object o2 = obj as object;
+
+			return o1 == o2;
+		}
+
+		/// <summary>
+		/// Operator == for objects of type GraphNode&lt;N, E&gt; and type N
+		/// </summary>
+		/// <param name="gn">First comparer</param>
+		/// <param name="obj">Second comparer</param>
+		/// <returns>True if equal, false otherwise</returns>
+		public static bool operator ==(N obj, GraphNode<N, E> gn)
+		{
+			return gn == obj;
+		}
+
+		/// <summary>
+		/// Operator != for objects of type GraphNode&lt;N, E&gt; and type N
+		/// </summary>
+		/// <param name="gn">First comparer</param>
+		/// <param name="obj">Second comparer</param>
+		/// <returns>True if not equal, false otherwise</returns>
+		public static bool operator !=(GraphNode<N, E> gn, N obj)
+		{
+			return !(gn == obj);
+		}
+
+		/// <summary>
+		/// Operator != for objects of type GraphNode&lt;N, E&gt; and type N
+		/// </summary>
+		/// <param name="gn">First comparer</param>
+		/// <param name="obj">Second comparer</param>
+		/// <returns>True if not equal, false otherwise</returns>
+		public static bool operator !=(N obj, GraphNode<N, E> gn)
+		{
+			return gn != obj;
+		}
+
+		/// <summary>
+		/// Determines whether the specified System.Object is equal to the current System.Object.
+		/// </summary>
+		/// <param name="obj">The System.Object to compare with the current System.Object.</param>
+		/// <returns>true if the specified System.Object is equal to the current System.Object; otherwise, false.</returns>
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj == null)
+			{
+				return false;
+			}
+
+			GraphNode<N, E> node = obj as GraphNode<N, E>;
+
+			if (node == null)
+			{
+				return false;
+			}
+
+			object o1 = node.Object as object;
+			object o2 = Object as object;
+
+			return o1 == o2;
+		}
+
+		/// <summary>
+		/// Serves as a hash function for a particular type.
+		/// </summary>
+		/// <returns>A hash code for the current System.Object.</returns>
+		public override int GetHashCode()
+		{
+			return Object.GetHashCode();
+		}
+
+		#endregion
+	}
+}
---
+++ TNT.Collections/Tests/GraphEdgeTests.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TNT.Collections;
+
+namespace Tests
+{
+	[TestClass]
+	public class GraphEdgeTests
+	{
+		[TestMethod]
+		public void OperatorEqualTest()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+
+			object no1 = new object();
+			object no2 = new object();
+			object no3 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+			GraphNode<object, object> gn3 = new GraphNode<object, object>(no3);
+
+
+			GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+			GraphEdge<object, object> ge2 = new GraphEdge<object, object>(eo1, gn1, gn2);
+			GraphEdge<object, object> ge3 = new GraphEdge<object, object>(eo2, gn2, gn3);
+
+			Assert.IsFalse(ge1 == null);
+			Assert.IsTrue(ge1 == ge1);
+			Assert.IsTrue(ge1 == ge2);
+			Assert.IsFalse(ge1 == ge3);
+
+			Assert.IsTrue(ge1 == eo1);
+			Assert.IsFalse(ge1 == eo2);
+
+			Assert.IsTrue(eo1 == ge1);
+			Assert.IsFalse(eo2 == ge2);
+
+			Assert.IsTrue(ge1 != null);
+			Assert.IsFalse(ge1 != ge1);
+			Assert.IsFalse(ge1 != ge2);
+			Assert.IsTrue(ge1 != ge3);
+
+			Assert.IsFalse(ge1 != eo1);
+			Assert.IsTrue(ge1 != eo2);
+
+			Assert.IsFalse(eo1 != ge1);
+			Assert.IsTrue(eo2 != ge2);
+		}
+
+		[TestMethod]
+		public void EqualsTest()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+
+			object no1 = new object();
+			object no2 = new object();
+			object no3 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+			GraphNode<object, object> gn3 = new GraphNode<object, object>(no3);
+
+
+			GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+			GraphEdge<object, object> ge2 = new GraphEdge<object, object>(eo1, gn1, gn2);
+			GraphEdge<object, object> ge3 = new GraphEdge<object, object>(eo2, gn2, gn3);
+
+			Assert.IsTrue(ge1.Equals(ge1));
+			Assert.IsTrue(ge1.Equals(ge2));
+			Assert.IsFalse(ge1.Equals(eo1));
+			Assert.IsFalse(ge1.Equals(null));
+			Assert.IsFalse(ge1.Equals(ge3));
+		}
+
+		[TestMethod]
+		public void GetHashCodeTest()
+		{
+			object eo1 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+			GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+
+			Assert.AreEqual(eo1.GetHashCode(), ge1.GetHashCode());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ConstructorTests()
+		{
+			object eo1 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+			try
+			{
+				new GraphEdge<object, object>(null, gn1, gn2);
+			}
+			catch (ArgumentNullException ane)
+			{
+				Assert.AreEqual("Value cannot be null.\r\nParameter name: obj", ane.Message);
+				throw;
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ConstructorTests1()
+		{
+			object eo1 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+			try
+			{
+				new GraphEdge<object, object>(eo1, null, gn2);
+			}
+			catch (ArgumentNullException ane)
+			{
+				Assert.AreEqual("Value cannot be null.\r\nParameter name: node1", ane.Message);
+				throw;
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ConstructorTests2()
+		{
+			object eo1 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+			try
+			{
+				new GraphEdge<object, object>(eo1, gn1, null);
+			}
+			catch (ArgumentNullException ane)
+			{
+				Assert.AreEqual("Value cannot be null.\r\nParameter name: node2", ane.Message);
+				throw;
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ConstructorTests3()
+		{
+			object eo1 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+			try
+			{
+				new GraphEdge<object, object>(eo1, gn1, gn1);
+			}
+			catch (ArgumentException ae)
+			{
+				Assert.AreEqual("Arguments node1 and node2 can not be the same node", ae.Message);
+				throw;
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void GetOppositeNodeTest()
+		{
+			object eo1 = new object();
+			object no1 = new object();
+			object no2 = new object();
+			object no3 = new object();
+
+			GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+			GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+			GraphNode<object, object> gn3 = new GraphNode<object, object>(no3);
+
+			GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+
+			try
+			{
+				ge1.GetOppositeNode(null);
+			}
+			catch (ArgumentNullException ane)
+			{
+				Assert.AreEqual("Value cannot be null.\r\nParameter name: node", ane.Message);
+				throw;
+			}
+		}
+		//	try
+		//	{
+		//		Assert.Throws<ArgumentException>(delegate { ge1.GetOppositeNode(gn3); });
+		//		ge1.GetOppositeNode(gn3);
+		//	}
+		//	catch (ArgumentException ae)
+		//	{
+		//		Assert.AreEqual("The node parameter is not one of the two nodes associated with this edge", ae.Message);
+		//	}
+
+		//	Assert.AreEqual(gn1, ge1.GetOppositeNode(gn2));
+		//	Assert.AreEqual(gn2, ge1.GetOppositeNode(gn1));
+		//}
+	}
+}
